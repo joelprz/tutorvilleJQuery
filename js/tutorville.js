ed0836cabc56240ec88046bf91bdcfb738f59d4c
Joel Przybylowski <przybylowski@Macintosh-2.local>
2016-07-10 10:46:05 -0500

    All but reset, styling, and responsiveness.

--- js/tutorville.js
+++ js/tutorville.js
@@ -4,13 +4,14 @@
       tutorTemplate,
       subjectsTemplate,
       tutorDataPromise,
-      searchResultSelected = false,
       selectedSubject,
-      shouldFilterBySubject = false,
-      shouldFilterByOnline = false,
-      shouldFilterByRadius = false,
+      isSearchResultSelected = false,
+      isFilteredBySubject = false,
+      isFilteredByOnline = false,
+      isFilteredByRadius = false,
+      isTutorListSorted = false,
+      sortedBy = {},
       filteredData = {},
-      sortResultsBy,
       $tutorResults,
       $subjectSearch,
       $subjectSearchResults,
@@ -35,78 +36,64 @@
         $subSearchBtnResults = $("#subSearchBtnResults");
         $subjectSearchDisplay = $("#subjectSearchDisplay");
         $selectRankPrice = $("#selectRankPrice");
-
         $isOnlineCheck = $("#isOnlineCheck");
         $isInRadiusCheck = $("#isInRadiusCheck");
-        
 
+         /* BEGIN SUBJECT_SEARCH */
 
-        $isOnlineCheck.on("change", function() {
-          shouldFilterByOnline = $(this).prop("checked");
-          filterTutors();
-        });
-
-        $isInRadiusCheck.on("change", function() {
-          shouldFilterByRadius = $(this).prop("checked");
-          filterTutors();
-        });
-
+        // Subject search input listener
         $subjectSearch.on("input", function(e) {
           searchSubjects(e.target.value);
         });
-
-        $selectRankPrice.on("change", function() {
-          switch ($selectRankPrice.val()) {
-            case "0":
-              sortResultsBy = null;
-              sortResults({});
-              break;
-            case "1": 
-              sortResultsBy = "sortHighRank";
-              sortResults({property: "Rank", highLow: "high"});
-              break;
-            case "2":
-              sortResultsBy = "sortLowRank";
-              sortResults({property: "Rank", highLow: "low"});
-              break;
-            case "3":
-              sortResultsBy = "sortHighPrice";
-              sortResults({property: "HourlyRate", highLow: "high"});
-              break;
-            case "4":
-              sortResultsBy = "sortLowPrice";
-              sortResults({property: "HourlyRate", highLow: "low"});
-              break;
-            default:
-              break;
-          }
-        });
         
-        $subSearchBtnResults.on("click", function(e) {
-          searchResultSelected = false;
+        // User selects from subjects search results
+        $subjectSearchResults.on("click", function(e) {
+          var subjectName = subjects[$(e.target).html()];
+          
+          selectedSubject = subjectName;
+          $subjectSearch.val(subjectName);
           displaySubjectResults({results:[]});
-          $subjectSearch.val(null);
-          toggleHide([$searchSubjects, $foundSubjects, $subSearchBtnResults, $subjectSearch]);
-          shouldFilterBySubject = false;
-          filterTutors();
+          toggleHide([$subjectSearchBtn]);
+          isSearchResultSelected = true;
         });
 
+        // Applies filtering by subject
         $subjectSearchBtn.on("click", function(e) {
-          var data = $.extend({}, tutorData);
-          
-          shouldFilterBySubject = true;
+          isFilteredBySubject = true;
           filterTutors();
           $subjectSearchDisplay.html(selectedSubject);
           toggleHide([$subjectSearchBtn, $searchSubjects, $foundSubjects, $subSearchBtnResults, $subjectSearch]);
         });
 
-        $subjectSearchResults.on("click", function(e) {
-          var subjectName = subjects[$(e.target).html()];
-          selectedSubject = subjectName;
-          $subjectSearch.val(subjectName);
+        // Remove subject filter and reset
+        $subSearchBtnResults.on("click", function(e) {
+          isSearchResultSelected = false;
           displaySubjectResults({results:[]});
-          toggleHide([$subjectSearchBtn]);
-          searchResultSelected = true;
+          $subjectSearch.val(null);
+          toggleHide([$searchSubjects, $foundSubjects, $subSearchBtnResults, $subjectSearch]);
+          isFilteredBySubject = false;
+          filterTutors();
+        });
+
+        /* END SUBJECT_SEARCH */
+        
+        $isOnlineCheck.on("change", function() {
+          isFilteredByOnline = $(this).prop("checked");
+          filterTutors();
+        });
+
+        $isInRadiusCheck.on("change", function() {
+          isFilteredByRadius = $(this).prop("checked");
+          filterTutors();
+        });
+
+        $selectRankPrice.on("change", function() {
+          var options = $selectRankPrice.val().split("-");
+          
+          sortedBy = {highLow: options[0], property: options[1]};
+          isTutorListSorted = true;
+          
+          sortResults(sortedBy);
         });
 
         parseTemplates();
@@ -117,50 +104,56 @@
         $.each(elems, function(index, value) {
           value.toggleClass('hide');  
         });
-
       },
 
       sortResults = function(options) {
-        var data;
+        var data = {};
 
-        if (options.property === "undefined") {
-          return;
-        }
-
-        if (shouldFilterBySubject || shouldFilterByOnline || shouldFilterByRadius) {
+        if (isFilteredBySubject || isFilteredByOnline || isFilteredByRadius) {
           data = dataParsers.sortTutors(filteredData, options);
-          displayTutorResults(data);
         } else {
-          data = dataParsers.sortTutors(tutorData, options);
-          displayTutorResults(data);
+          data = dataParsers.sortTutors($.extend({}, tutorData), options);
         }
+
+        displayTutorResults(data);
       },
 
       filterTutors = function() {
         var data = $.extend({}, tutorData);
         
         // This can be made more elegant
-        data = shouldFilterBySubject ? dataParsers.tutorsForSubject(data, selectedSubject) : data;
-        data = shouldFilterByOnline ? dataParsers.tutorsOnline(data) : data;
-        data = shouldFilterByRadius ? dataParsers.tutorsRadius(data) : data;
-        
-        filteredData = data;
+        data = isFilteredBySubject ? dataParsers.tutorsForSubject(data, selectedSubject) : data;
+        data = isFilteredByOnline ? dataParsers.tutorsOnline(data) : data;
+        data = isFilteredByRadius ? dataParsers.tutorsRadius(data) : data;
 
+        if (isTutorListSorted) {
+          data = dataParsers.sortTutors(data, sortedBy);
+        }
+
+        filteredData = data;
         displayTutorResults(data);
       },
 
       resetFilters = function() {
-        shouldFilterBySubject = true;
-        shouldFilterByOnline = true;
-        shouldFilterByRadius = true;
+        isFilteredBySubject = false;
+        isFilteredByOnline = false;
+        isFilteredByRadius = false;
+
+        isTutorListSorted = false;
+        sortedBy = {};
+
+        // reset sort menu and switch boolean
+        // redraw view with tutorData
+
+        // MAKE SURE $selectRankPrice.on( is catching null value when reset
       },
 
       searchSubjects = function(srchStr) {
         var subject = srchStr.toLowerCase() || null,
             results = [];
         
-        if (searchResultSelected) {
-          searchResultSelected = false;
+        if (isSearchResultSelected) {
+          isSearchResultSelected = false;
           $subjectSearchBtn.toggleClass('hide')
         }
         
@@ -215,6 +208,7 @@
       displayTutorResults = function(d) {
         d.SeachCount = d.SearchResults.length;
         d.TutorCountIsNotOne = d.SearchResults.length !== 1;
+        d.SubjectFilter = isSearchResultSelected ? selectedSubject : "";
         $tutorResults.html(Mustache.render(tutorTemplate, d));
       },
 
@@ -234,8 +228,8 @@
         },
         
         tutorsForSubject: function(data, subject) {
-          var length = data.SearchResults.length;
-          var tutors = [];
+          var length = data.SearchResults.length,
+              tutors = [];
           
           for (var i=0; i < length; i++) {
             if ( $.inArray(subject, data.SearchResults[i].Subjects) !== -1 ) {
@@ -249,8 +243,8 @@
         },
 
         tutorsOnline: function(data) {
-          var length = data.SearchResults.length;
-          var tutors = [];
+          var length = data.SearchResults.length,
+              tutors = [];
           
           for (var i=0; i < length; i++) {
             if (data.SearchResults[i].IsOnline) {
@@ -264,8 +258,8 @@
         },
 
         tutorsRadius: function(data) {
-          var length = data.SearchResults.length;
-          var tutors = [];
+          var length = data.SearchResults.length,
+              tutors = [];
           
           for (var i=0; i < length; i++) {
             if (data.SearchResults[i].Distance <= 5) {
@@ -278,15 +272,7 @@
           return data;
         },
 
-        unsorted: function(data) {
-
-        },
-
         sortTutors: function(data, options) {
-          if (options.property === "undefined") {
-            alert("undefined");
-          }
-
           data.SearchResults.sort(function(a, b) {
               return options.highLow === "low" ? parseFloat(a[options.property]) - parseFloat(b[options.property]) :
                 parseFloat(b[options.property]) - parseFloat(a[options.property]);
